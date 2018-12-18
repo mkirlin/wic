0625e9898ee00f21dece2ba4900521d68bf9de7a
Michael Kirlin <michael.kirlin@here.com>
2018-12-18 02:11:47 -0600

    Updating configs

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,22 @@
 
 Rails.application.routes.draw do
   root 'base#index'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+
+
   get 'search', to: 'base#search_results'
   post 'search', to: 'base#search'
-  devise_for :users
 
   scope module: 'v1', path: 'v1' do
     resources :users
     resources :events
-    resources :chat_rooms, only: [:new, :create, :show, :index]
   end
 
   scope :admin do
     get 'csv-export', to: 'admin#csv_export'
+  end
+
+  if Rails.env.production?
+    get '*path' => redirect('/')
   end
 end
